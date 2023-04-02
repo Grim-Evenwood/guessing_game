6e5bde84b1fd59996e2b678ce666f4806fae4b29
Rachel Pang <rlpang6@gmail.com>
2023-04-02 17:13:46 -0500

    New project, finished

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the numbew, bitch 🤔🤔!");
+    let secret_number = rand::thread_rng().gen_range(69..=420);
+    // println!("The secwet numbew iws uwu:{secret_number}, Bitch");
+
+    loop {
+        println!("Pwease input youw guess, Bitch");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faiwed tuwu wead wine, Bitch~😒😒😒😒😒");
+
+        let guess: u32 = match guess.
+            trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => continue, 
+            };        
+            println!("Uwu guessed: {guess}, Bitch 🤦‍♀️🤦‍♀️");
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("too smaww 😭😭!"),
+                Ordering::Greater => println!("too wawge 👀👀!"),
+                Ordering::Equal => {
+                    println!("congwatuwations bitch! 😍😍😍👌👌👌👌👌\nduwu uwu wawnt a pwize??~🙀🙀🙀");
+                    println!("uwu win !!!!!!");
+                    break;
+                }
+            }
+    };
+    
+    }
